decb01d3298822b88a68a9ced0eabf4f6f1117f1
artemm3708 <artemm37.08@gmail.com>
2021-12-02 00:13:02 +0200

    Add lesson 2

---
+++ src/com/hillel/classwork/lesson2/Main.java
@@ -0,0 +1,13 @@
+package com.hillel.classwork.lesson2;
+
+public class Main {
+
+    // psvm
+    public static void main(String[] args) {
+
+
+        //sout
+        System.out.println("Hello in Idea");
+    }
+
+}
---
+++ src/com/hillel/classwork/lesson2/Variables.java
@@ -0,0 +1,46 @@
+package com.hillel.classwork.lesson2;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        byte myByte = 101;
+        System.out.println(myByte);
+
+        short myShort = 30000;
+        System.out.println("this is short: " + myShort);
+
+        int result = myByte + myShort;
+        System.out.println(result);
+
+        int myInt = 478_272_983;
+        System.out.println(myInt);
+
+        long myLong = 129_372_344_374_292L;
+        System.out.println(myLong);
+
+        float myFloat = 659.346F;
+        System.out.println(myFloat);
+
+        double myDouble = 687290.92749240;
+        System.out.println(myDouble);
+
+        boolean isValid = false;
+        System.out.println(isValid);
+
+        char myChar = '~';
+        System.out.println(myChar);
+
+        char ch2 = 800;
+        System.out.println(ch2);
+
+        char ch3 = '\u270C';
+        System.out.println(ch3);
+
+        String string = "test";
+        System.out.println(string);
+
+
+    }
+
+}
