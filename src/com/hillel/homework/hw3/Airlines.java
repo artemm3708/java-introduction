1a154fae4ae1f84dc366313155877ab718cbacf5
artemm3708 <artemm37.08@gmail.com>
2021-12-11 19:42:32 +0200

    add hw3

--- src/com/hillel/homework/hw3/Airlines.java
+++ src/com/hillel/homework/hw3/Airlines.java
@@ -14,35 +14,34 @@ public class Airlines {
         String namePlane1 = reader.readLine();
         System.out.println("Введите тип самолета: ");
         String typePlane1 = reader.readLine();
+        System.out.println(printAirplaneInfo(namePlane1, typePlane1));
 
         String namePlane2 = "Airbus";
         System.out.println("Введите модель самолета Airbus чтобы узнать доп. информацию: ");
         String typePlane2 = reader.readLine();
         int economyClass = 100;
+        System.out.println(printAirplaneInfo(namePlane2, typePlane2, economyClass));
 
         String namePlane3 = "Boeing";
         System.out.println("Введите модель самолета Boeing чтобы узнать доп. информацию: ");
         String typePlane3 = reader.readLine();
         int economyClass2 = 130;
         int businessClass = 30;
-
-        System.out.println(printAirplaneInfo("Тип самолета: " + namePlane1, ", модель самолета: " + typePlane1));
-        System.out.println(printAirplaneInfo("Тип самолета: " + namePlane2, ", модель самолета: " + typePlane2 + ", кол-во мест эконом класса: " + economyClass2));
-        System.out.println(printAirplaneInfo("Тип самолета: " + namePlane3, ", модель самолета: " + typePlane3 + ", кол-во мест эконом класса: " + economyClass2 + ", кол-во мест бизнесс класса: " + businessClass));
+        System.out.println(printAirplaneInfo(namePlane3, typePlane3, economyClass2, businessClass));
     }
 
     public static String printAirplaneInfo(String namePlane, String typePlane) {
-        String plane1 = namePlane + typePlane;
+        String plane1 = "Тип самолета: " +  namePlane + ", модель самолета: " + typePlane;
         return plane1;
     }
 
     public static String printAirplaneInfo(String namePlane, String typePlane, int economyClass) {
-        String plane2 = namePlane + typePlane + economyClass;
+        String plane2 = "Тип самолета: " +  namePlane + ", модель самолета: " + typePlane + ", кол-во мест эконом класса: " + economyClass;
         return plane2;
     }
 
     public static String printAirplaneInfo(String namePlane, String typePlane, int economyClass, int businessClass) {
-        String plane3 = namePlane + typePlane + economyClass + businessClass;
+        String plane3 = "Тип самолета: " +  namePlane + ", модель самолета: " + typePlane + ", кол-во мест эконом класса: " + economyClass + ", кол-во мест бизнесс класса: " + businessClass ;
         return plane3;
     }
 
