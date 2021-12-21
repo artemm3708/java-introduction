80774f5564397abe8911567a9fc96da803af32f1
artemm3708 <artemm37.08@gmail.com>
2021-12-21 23:46:34 +0200

    add hw

--- src/com/hillel/homework/hw6/UnluckyNumbers.java
+++ src/com/hillel/homework/hw6/UnluckyNumbers.java
@@ -8,6 +8,10 @@ public class UnluckyNumbers {
         for (i = 0; i <= 100; i++) {
             if (i % 10 == 4 || i % 10 == 9) {
                 continue;
+            } if (i >= 40 && i < 50) {
+                continue;
+            } if (i >= 90 && i < 100) {
+                continue;
             }
             System.out.println(i);
         }
