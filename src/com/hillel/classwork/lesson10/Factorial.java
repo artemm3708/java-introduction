5cd47446d3443c3b75299a4588ae5c0e08c1f546
artemm3708 <artemm37.08@gmail.com>
2021-12-29 20:00:52 +0200

    add hw

---
+++ src/com/hillel/classwork/lesson10/Factorial.java
@@ -0,0 +1,20 @@
+package com.hillel.classwork.lesson10;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        System.out.println(factorial(5));
+
+
+    }
+
+    public static int factorial(int number) {
+        if (number == 1) {
+            return number;
+        }
+        return number * factorial(number - 1);
+
+    }
+
+}
---
+++ src/com/hillel/classwork/lesson10/Task1.java
@@ -0,0 +1,32 @@
+package com.hillel.classwork.lesson10;
+
+import java.util.Arrays;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+
+        String str = "*";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(str);
+            str = str + "*";
+        }
+
+
+    }
+
+}
